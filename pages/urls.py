f940a1a6970b479b866a7fe1c9117cfd86f7f61a
Mahdi Hamzei <hamzei8434@gmail.com>
2025-11-19 21:37:29 +0330

    add about_us view

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('about_us/', views.AboutUsView.as_view(), name='about_us'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class HomePageView(generic.TemplateView):
     template_name = 'home.html'
+
+class AboutUsView(generic.TemplateView):
+    template_name = 'about_us.html'
+    
