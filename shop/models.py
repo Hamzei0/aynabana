1f9883d4e414e5b341a3a1eb1e140a6fee50f7ca
Mahdi Hamzei <hamzei8434@gmail.com>
2025-11-28 00:32:27 +0330

    add comment model for products

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
-
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveManager,self).get_queryset().filter(active=True)
+    
 class Products(models.Model):
     
     title = models.CharField(max_length=100)
@@ -24,3 +28,42 @@ class Products(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args={self.pk})
 
+
+class CommentProduct(models.Model):
+    PRODUCT_STARS = [
+        ('1','very bad'),
+        ('2','bad'),
+        ('3','normal'),
+        ('4','good'),
+        ('5','perfect'),
+    ]
+    
+    product = models.ForeignKey(
+        Products, 
+        on_delete=models.CASCADE, 
+        related_name='comments',
+        )
+    author = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        related_name='comments',
+        )
+    
+    text = models.TextField()
+    active = models.BooleanField(default=False)
+    stars = models.CharField(
+        max_length=10, 
+        choices=PRODUCT_STARS,
+        )
+    
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    
+    # manager
+    objects = models.Manager()
+    comment_filter = ActiveManager()
+    
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
+    
