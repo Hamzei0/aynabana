f884f4a2d81b8d61a62345ac2b399f4a88ed6972
Mahdi Hamzei <hamzei8434@gmail.com>
2025-11-30 21:34:08 +0330

    add comment form for product detail

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
+from . import forms
 from . import models
 
 class ProductListView(generic.ListView):
@@ -12,4 +14,25 @@ class ProductDetailView(generic.DetailView):
     model = models.Products
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = forms.CommentForm()
+        return context
+
+class CommentCreate(generic.CreateView):
+    model = models.CommentProduct
+    form_class = forms.CommentForm
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        
+        product_id = int(self.kwargs['product_id'])
+        product = get_object_or_404(models.Products, id=product_id)   
+        
+        obj.product = product 
+        
+        return super().form_valid(form)
+        
     
