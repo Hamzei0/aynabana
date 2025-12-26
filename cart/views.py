09701bcaf17e859b7e694d5bdc8a1f8a0bacdfcb
Hamzei0 <hamzei8434@gmail.com>
2025-12-26 13:27:53 +0330

    Build and develop cart section

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .cart import Cart
+from .forms import AddToCartProductForm
+
+from shop.models import Products
+
+
+def cart_detail_view(request):
+
+    cart = Cart(request)
+
+    for item in cart:
+        item["product_update_quantity_form"] = AddToCartProductForm(
+            initial={
+                "quantity": item["quantity"],
+                "inplace": True,
+            }
+        )
+
+    return render(request, "cart/cart_detail.html", context={"cart": cart})
+
+
+@require_POST
+def add_to_cart_view(request, product_id):
+
+    cart = Cart(request)
+
+    product = get_object_or_404(Products, pk=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data["quantity"]
+        cart.add(product, quantity, replace_current_quantity=cleaned_data["inplace"])
+
+    return redirect("cart:cart_detail")
+
+
+@require_POST
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Products, pk=product_id)
+
+    cart.remove(product)
+    return redirect("cart:cart_detail")
+
+
+def cart_clear(request):
+    cart = Cart()
+
+    if len(cart):
+        cart.clear()
+
+    return redirect("cart:cart_detail")
+
+
+def cart_clear(request):
+    cart = Cart(request)
+
+    if len(cart):
+        cart.clear()
+
+    return redirect("cart:cart_detail")
