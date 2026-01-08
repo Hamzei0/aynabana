26e94b4c440ccd0c27d705886df633a366617f21
Hamzei0 <hamzei8434@gmail.com>
2026-01-08 16:33:43 +0330

    add template view for login & signup

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
