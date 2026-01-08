8ee9992c16f9070ba4e88a5507bfb34bc5ec90fd
Hamzei0 <hamzei8434@gmail.com>
2026-01-08 18:56:05 +0330

    create privacy_policy page

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('about_us/', views.AboutUsView.as_view(), name='about_us'),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("about_us/", views.AboutUsView.as_view(), name="about_us"),
+    path("privacy_policy/", views.PrivacyPolicy.as_view(), name="privacy_policy"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 class HomePageView(generic.TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class AboutUsView(generic.TemplateView):
-    template_name = 'about_us.html'
-    
+    template_name = "about_us.html"
+
+
+class PrivacyPolicy(generic.TemplateView):
+    template_name = "privacy_policy.html"
