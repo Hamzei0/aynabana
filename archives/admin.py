8a5503fc11029ff0cd154350f42d66131b60936c
Hamzei0 <hamzei8434@gmail.com>
2026-01-10 04:46:28 +0330

    create comment_archive model

--- archives/admin.py
+++ archives/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from . import models
 
 
+class CommentsInLine(admin.TabularInline):
+    model = models.CommentArchive
+    fields = ["text", "author", "stars", "active"]
+    extra = 1
+
+
 @admin.register(models.Archives)
 class ArchiveAdmin(admin.ModelAdmin):
     list_display = ["author", "title", "active"]
+    inlines = [
+        CommentsInLine,
+    ]
+
+
+@admin.register(models.CommentArchive)
+class CommentArchiveAdmin(admin.ModelAdmin):
+    list_display = ["author", "archive", "text", "stars", "active"]
--- archives/models.py
+++ archives/models.py
@@ -5,6 +5,11 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
 
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveManager, self).get_queryset().filter(active=True)
+
+
 class Archives(models.Model):
     author = models.ForeignKey(
         get_user_model(),
@@ -37,3 +42,58 @@ class Archives(models.Model):
 
     def get_absolute_url(self):
         return reverse("archive_detail", args=[self.pk])
+
+
+class CommentArchive(models.Model):
+    Archive_STARS = [
+        ("1", _("very bad")),
+        ("2", _("bad")),
+        ("3", _("normal")),
+        ("4", _("good")),
+        ("5", _("perfect")),
+    ]
+
+    archive = models.ForeignKey(
+        Archives,
+        on_delete=models.CASCADE,
+        related_name="archive_comments",
+        verbose_name="archive",
+    )
+
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="archive_author",
+        verbose_name=_("author"),
+    )
+
+    text = models.TextField(verbose_name=_("text"))
+    active = models.BooleanField(
+        default=False,
+        verbose_name=_("active"),
+    )
+    stars = models.CharField(
+        max_length=1,
+        choices=Archive_STARS,
+        verbose_name=_("stars"),
+    )
+
+    datetime_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("datetime created"),
+    )
+    datetime_modified = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("datetime modified"),
+    )
+
+    # manager
+    objects = models.Manager()
+    comment_filter = ActiveManager()
+
+    class Meta:
+        verbose_name = _("CommentArchive")
+        verbose_name_plural = _("CommentArchives")
+
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.product.id])
