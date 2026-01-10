1be2310d987805cbf8141921eca10b187486a392
Hamzei0 <hamzei8434@gmail.com>
2026-01-10 14:23:09 +0330

    create consulting model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Consulting(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name=_("first name"))
+    last_name = models.CharField(max_length=30, verbose_name=_("last name"))
+
+    city = models.CharField(max_length=70, verbose_name=_("city"))
+    note = models.TextField(verbose_name=_("note"))
+
+    datetime_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("datetime created"),
+    )
+    datetime_modified = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("datetime modified"),
+    )
