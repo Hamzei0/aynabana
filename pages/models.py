d4a6d2a358e23283515b1ceac0aa7a074252e08a
Hamzei0 <hamzei8434@gmail.com>
2026-02-12 02:16:48 +0330

    Add multiple fields to the Consulting model

--- pages/models.py
+++ pages/models.py
@@ -4,10 +4,36 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Consulting(models.Model):
+
+    SERVICE_CHOICES = [
+        ("cabinet", _("Cabinet")),
+        ("tile", _("Tile")),
+        ("decoration", _("Decoration Change")),
+        ("knauf", _("Knauf")),
+        ("renovation", _("Full Renovation")),
+    ]
+
+    CONTACT_TIME_CHOICES = [
+        ("morning", _("Morning")),
+        ("noon", _("Noon")),
+        ("evening", _("Evening")),
+    ]
+
     first_name = models.CharField(max_length=30, verbose_name=_("first name"))
     last_name = models.CharField(max_length=30, verbose_name=_("last name"))
 
     city = models.CharField(max_length=70, verbose_name=_("city"))
+
+    area_size = models.PositiveIntegerField(default=0, verbose_name=_("area size"))
+
+    service_type = models.CharField(
+        max_length=20, choices=SERVICE_CHOICES, verbose_name=_("service type")
+    )
+
+    contact_time = models.CharField(
+        max_length=10, choices=CONTACT_TIME_CHOICES, verbose_name=_("contacttime")
+    )
+
     note = models.TextField(verbose_name=_("note"))
 
     datetime_created = models.DateTimeField(
