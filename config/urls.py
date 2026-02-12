fa6a7a4c1c152b2d13434af730afb62677ec2f9c
Hamzei0 <hamzei8434@gmail.com>
2026-02-12 22:40:14 +0330

    Making the website bilingual

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("rosetta/", include("rosetta.urls")),
+]
+
+urlpatterns += i18n_patterns(
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("shop/", include("shop.urls")),
@@ -30,9 +35,8 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("articles/", include("articles.urls")),
     path("order/", include("orders.urls")),
-    # rosetta (i18n)
-    path("rosetta/", include("rosetta.urls")),
-]
+    prefix_default_language=False,
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
