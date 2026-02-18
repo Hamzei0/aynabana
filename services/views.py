35e095b01f709f288183a8b0cb0fc196767c9f0c
Hamzei0 <hamzei8434@gmail.com>
2026-02-18 13:37:10 +0330

    show image in services page

--- services/views.py
+++ services/views.py
@@ -8,3 +8,4 @@ class Services(generic.ListView):
     model = models.Service
     template_name = "services/services.html"
     context_object_name = "services"
+    ordering = ["-datetime_created"]
